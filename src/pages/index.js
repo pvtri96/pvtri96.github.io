ccc926e8e80e3d8694a0c395a9c67becfa8ccc42
Tri Van Pham <pvtri96@gmail.com>
2021-03-20 01:02:08 +0700

    fix: breaking stuffs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Layout, SEO } from 'components/common';
 import { Intro, Skills, Contact, Projects } from 'components/landing';
 
-export default () => (
-  <Layout>
-    <SEO />
-    <Intro />
-    <Projects />
-    <Skills />
-    <Contact />
-  </Layout>
-);
+export default function MainPage() {
+  return (
+    <Layout>
+      <SEO />
+      <Intro />
+      <Projects />
+      <Skills />
+      <Contact />
+    </Layout>
+  );
+}
