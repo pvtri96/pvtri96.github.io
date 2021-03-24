6e0684a8ca18ae1b23d9357c5654455ca1b6920b
Tri Pham <pvtri96@gmail.com>
2021-03-24 10:47:12 +0700

    fix: dont use gatsby-plugin-image for intro and theme toggler

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -1,12 +1,11 @@
+import dev from 'assets/illustrations/dev.svg';
 import { Button, Container } from 'components/common';
 import { Header } from 'components/theme';
-import { StaticImage } from 'gatsby-plugin-image';
 import { ThemeContext } from 'providers/ThemeProvider';
 import React, { useContext } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Details, IntroWrapper, Thumbnail, Wrapper } from './styles';
 
-const BASE_ASSETS_DIR = "../../../assets";
 export const Intro = () => {
   const { theme } = useContext(ThemeContext);
 
@@ -22,7 +21,7 @@ export const Intro = () => {
           </Button>
         </Details>
         <Thumbnail>
-          <StaticImage src={`${BASE_ASSETS_DIR}/illustrations/dev.svg`} alt="I’m Tri and I’m a Frontend & Devops engineer!" />
+          <img src={dev} alt="I’m Tri and I’m a Frontend & Devops engineer!" />
         </Thumbnail>
       </IntroWrapper>
     </Wrapper>
--- src/components/theme/Header/ToggleTheme/index.jsx
+++ src/components/theme/Header/ToggleTheme/index.jsx
@@ -1,27 +1,15 @@
-import { StaticImage } from "gatsby-plugin-image";
-import { ThemeContext } from "providers/ThemeProvider";
-import React, { useContext } from "react";
-import { Wrapper } from "./styles";
+import React, { useContext } from 'react';
+import { ThemeContext } from 'providers/ThemeProvider';
+import sunIcon from 'assets/icons/sun.svg';
+import moonIcon from 'assets/icons/moon.svg';
+import { Wrapper } from './styles';
 
-const BASE_ASSETS_DIR = "../../../../assets";
 const ToggleTheme = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
     <Wrapper type="button" onClick={toggleTheme}>
-      {theme === "light" ? (
-        <StaticImage
-          src={`${BASE_ASSETS_DIR}/icons/moon.svg`}
-          alt={theme}
-          theme={theme}
-        />
-      ) : (
-        <StaticImage
-          src={`${BASE_ASSETS_DIR}/icons/sun.svg`}
-          alt={theme}
-          theme={theme}
-        />
-      )}
+      <img src={theme === 'light' ? moonIcon : sunIcon} alt={theme} theme={theme} />
     </Wrapper>
   );
 };
