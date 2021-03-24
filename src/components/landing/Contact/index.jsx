bf587599ccc90eba4ed21a74aa0c91d8322d6aa8
Tri Pham <pvtri96@gmail.com>
2021-03-24 13:53:30 +0700

    fix: avoid using StaticImage for svg

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -1,10 +1,9 @@
+import contact from 'assets/illustrations/contact.svg';
 import { Container } from 'components/common';
-import { StaticImage } from "gatsby-plugin-image";
 import React from 'react';
 import ContactForm from './ContactForm';
 import { Details, Thumbnail, Wrapper } from './styles';
 
-const BASE_ASSETS_DIR = "../../../assets"
 export const Contact = () => (
   <Wrapper as={Container} id="contact">
     <Details>
@@ -12,7 +11,7 @@ export const Contact = () => (
       <ContactForm />
     </Details>
     <Thumbnail>
-      <StaticImage src={`${BASE_ASSETS_DIR}/illustrations/contact.svg`} alt="Contact me" />
+      <img src={contact} alt="Contact me" />
     </Thumbnail>
   </Wrapper>
 );
