506dfa9830398d7caa49390fecbf5db1762ab967
pvtri96 <pvtri96@users.noreply.github.com>
2022-09-20 11:00:19 +0000

    deploy: 141c7c533fecfb0def9baaf8d7efeeb9cf448bd1

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-48896e4a3ccb6aa2c7c4.js"
+    "url": "webpack-runtime-bca755a12de9187e2193.js"
   },
   {
     "url": "framework-f92586ccbe9e1bf07e6e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8d80673cea67143a31f6126c82802155"
+    "revision": "6a445ef75e3f9169f6aebf0378cac43d"
   },
   {
     "url": "polyfill-143d49d662020a2c412a.js"
